3e82e08b59f075abe01c093140fc58e0f88b6f57
PVabulas <phil.vabulas@london.gov.uk>
2024-03-14 18:20:47 +0000

    Catch up on work to date.

--- Day20 - Snake/game.py
+++ Day20 - Snake/game.py
@@ -5,39 +5,45 @@ from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
 
-speed_dict = {"fast": 0.05, "normal": 0.1, "slow": 0.2}
 
-t.colormode(255)
-screen = Screen()
-screen_width = 600
-screen_height = 600
-screen.setup(width=screen_width, height=screen_height)
-screen.bgcolor("black")
-screen.title("Snake")
-speed = speed_dict[
-    screen.textinput("Select game spped", "Fast, Normal, or Slow?").lower()
-]
-screen.tracer(n=0)
-screen.listen()
+def run_game():
+    speed_dict = {"fast": 0.05, "normal": 0.1, "slow": 0.2}
 
-snake = Snake(screen, size=20, speed=speed)
-food = Food(snake.size)
-score = Scoreboard()
-screen.update()
+    t.colormode(255)
+    screen = Screen()
+    screen_width = 600
+    screen_height = 600
+    screen.setup(width=screen_width, height=screen_height)
+    screen.bgcolor("black")
+    screen.title("Snake")
+    speed = speed_dict[
+        screen.textinput("Select game speed", "Fast, Normal, or Slow?").lower()
+    ]
+    screen.tracer(n=0)
+    screen.listen()
 
-is_game_on = True
-while is_game_on:
-    snake.move()
-    if snake.head.distance(food) < 5:
-        food.refresh()
-        score.increase_score()
-        snake.add_body()
-    if snake.detect_collision(screen_width, screen_height):
-        is_game_on = False
+    snake = Snake(screen, size=20, speed=speed)
+    food = Food(snake.size)
+    score = Scoreboard()
+    screen.update()
 
-game_over = Turtle(visible=False)
-game_over.color("white")
-game_over.write("Game Over", align="center", font=("Courier", 24, "normal"))
-screen.update()
+    is_game_on = True
+    while is_game_on:
+        snake.move()
+        if snake.head.distance(food) < 5:
+            food.refresh()
+            score.increase_score()
+            snake.add_body()
+        if snake.detect_collision(screen_width, screen_height):
+            is_game_on = False
 
-screen.exitonclick()
+    game_over = Turtle(visible=False)
+    game_over.color("white")
+    game_over.write("Game Over", align="center", font=("Courier", 24, "normal"))
+    screen.update()
+
+    screen.exitonclick()
+
+
+if __name__ == "__main__":
+    run_game()
---
+++ Day22 - Pong/pong.py
@@ -0,0 +1,164 @@
+# Screen
+# Net
+# Paddle(s)
+# Ball
+# Scoreboard(s)
+
+from logging.config import _LoggerConfiguration
+from math import sqrt
+import turtle as t
+from turtle import Screen, Turtle
+from time import sleep
+
+
+def run_game():
+    speed_dict = {"fast": 0.05, "normal": 0.1, "slow": 0.2}
+
+    t.colormode(255)
+    screen = Screen()
+    screen_width = 1200
+    screen_height = 600
+    screen.setup(width=screen_width, height=screen_height)
+    screen.bgcolor("black")
+    screen.title("Pong")
+    #    speed = speed_dict[
+    #        screen.textinput("Select game speed", "Fast, Normal, or Slow?").lower()
+    #    ]
+    screen.tracer(n=0)
+    screen.listen()
+
+    game = Game(screen_width, screen_height)
+    screen.onkey(game.left_paddle.move_up, "w")
+    screen.onkey(game.left_paddle.move_down, "s")
+    screen.onkey(game.right_paddle.move_up, "Up")
+    screen.onkey(game.right_paddle.move_down, "Down")
+    screen.onkey(game.quit_game, key="q")
+
+    while game.game_is_on:
+        screen.update()
+
+    screen.exitonclick()
+
+
+class Game:
+    game_is_on = True
+
+    def __init__(self, screen_width, screen_height) -> None:
+        Net(screen_height)
+        self.left_paddle = Paddle(screen_width, "left")
+        self.right_paddle = Paddle(screen_width, "right")
+        self.ball = Ball(screen_width)
+        self.left_score = Scoreboard(screen_height, "left")
+        self.right_score = Scoreboard(screen_height, "right")
+
+    def quit_game(self):
+        self.game_is_on = False
+        print("Game is off")
+
+
+class Net(Turtle):
+    def __init__(self, screen_height) -> None:
+        super().__init__(visible=False)
+        self.penup()
+        self.goto(5, int(screen_height / 2))
+        self.setheading(270)
+        self.color("white")
+        for i in range(int(screen_height / 50)):
+            self.pendown()
+            self.begin_fill()
+            self.forward(25)
+            self.right(90)
+            self.forward(10)
+            self.right(90)
+            self.forward(25)
+            self.right(90)
+            self.forward(10)
+            self.right(90)
+            self.end_fill()
+            self.penup()
+            self.forward(50)
+
+
+class Paddle:
+    def __init__(self, screen_width, side):
+        if side == "left":
+            x_pos = -(int(screen_width / 2) - 20)
+        elif side == "right":
+            x_pos = int(screen_width / 2) - 20
+        else:
+            raise AttributeError("side must be 'left' or 'right'")
+
+        self.paddle = [Turtle("square") for _ in range(5)]
+        for i, part in enumerate(self.paddle):
+            part.penup()
+            part.color("white")
+            y_pos = -40 + i * 20
+            part.goto(x_pos, y_pos)
+
+    def move_up(self):
+        for part in self.paddle:
+            part.setheading(90)
+            part.forward(20)
+
+    def move_down(self):
+        for part in self.paddle:
+            part.setheading(270)
+            part.forward(20)
+
+
+class Ball(Turtle):
+    def __init__(self, screen_width) -> None:
+        super().__init__("square")
+        self.penup()
+        self.color("white")
+        self.goto(-int(screen_width / 2) + 40, 0)
+        self.seth(45)
+
+    def move_ball(self):
+        self.forward(sqrt(2*20**2))
+
+    def detect_collision(self, screen_width, screen_height, left_paddle, right_paddle):
+        left_paddle_y_cors = [section.y_cor() for section in left_paddle]
+        right_paddle_y_cors = [section.y_cor() for section in right_paddle]
+        ball_on_paddle_y_cor = []
+        for cor in left_paddle_y_cors:
+            if abs(self.ycor - cor) <= 5:
+                ball_on_paddle_y_cor.append("left")
+        for cor in right_paddle_y_cors:
+            if abs(self.ycor - cor) <= 5:
+                ball_on_paddle_y_cor.append("right")
+                
+        if self.xcor() <= -(int(screen_width / 2) - 25):
+            return "left_wall"
+        elif self.xcor() >= int(screen_width / 2) - 25:
+            return "right_wall"
+        elif self.xcor() <= -(int(screen_width / 2) - 45) and "left" in ball_on_paddle_y_cor:
+
+
+class Scoreboard(Turtle):
+    def __init__(self, screen_height, side) -> None:
+        super().__init__(visible=False)
+        self.color("white")
+        self.score = 0
+        self.penup()
+        if side == "left":
+            x_pos = -100
+            self.align = "Right"
+        elif side == "right":
+            x_pos = 100
+            self.align = "Left"
+        else:
+            raise AttributeError("side must be 'left' or 'right'")
+        y_pos = int(screen_height / 2) - 150
+        self.goto(x_pos, y_pos)
+        self.pendown()
+        self.write(f"{self.score}", font=("Bit5x3", 100, "normal"), align=self.align)
+
+    def increase_score(self):
+        self.undo()
+        self.score += 1
+        self.write(f"{self.score}", font=("Bit5x3", 100, "normal"), align=self.align)
+
+
+if __name__ == "__main__":
+    run_game()
